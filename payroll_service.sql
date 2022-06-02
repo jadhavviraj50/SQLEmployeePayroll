4eab4811cc711d36d339f83b6c4d389fdaab9f12
Viraj <jadhavviraj50@gmail.com>
2022-06-02 18:58:45 +0530

    [Viraj]-Added UC2 File

--- payroll_service.sql
+++ payroll_service.sql
@@ -1,4 +1,15 @@
+UC1 :
+
 CREATE DATABASE payroll_service
 use payroll_service;
 
+UC2 :
+
+CREATE TABLE employee_payroll 
+(
+id INT PRIMARY KEY IDENTITY (1,1),
+name VARCHAR (150),
+salary INT,
+startdate DATE,
+);
 
