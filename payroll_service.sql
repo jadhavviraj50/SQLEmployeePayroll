3dfd093f72575ba897ad2a7c82dee73f8d8ccf9f
Viraj <jadhavviraj50@gmail.com>
2022-06-02 19:34:17 +0530

    [Viraj]-Added UC3-Create Employee Data file

--- payroll_service.sql
+++ payroll_service.sql
@@ -1,15 +1,24 @@
-UC1 :
+UC1 : To create a payroll service database
 
 CREATE DATABASE payroll_service
 use payroll_service;
 
-UC2 :
+UC2 : To create employee_payroll table
 
 CREATE TABLE employee_payroll 
 (
-id INT PRIMARY KEY IDENTITY (1,1),
+id INT IDENTITY(1,1) PRIMARY KEY ,
 name VARCHAR (150),
 salary INT,
 startdate DATE,
 );
+
+UC3 : To create employee payroll data
+
+insert into employee_payroll values('Viraj',25000,'2022-04-14'),('Vaibhav',60000,'2021-08-15'),('Varad',15000,'2023-04-20');
+
+
+
+
+
 
