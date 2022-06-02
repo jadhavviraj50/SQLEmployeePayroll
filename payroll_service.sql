d039539df083390c01b1ffd37afe4a3a9350606b
Viraj <jadhavviraj50@gmail.com>
2022-06-02 22:10:07 +0530

    [Viraj]-Added UC7-Ability to find Sum Avg Min Max Count file

--- payroll_service.sql
+++ payroll_service.sql
@@ -15,7 +15,7 @@ startdate DATE,
 
 UC3 : To create employee_payroll data
 
-insert into employee_payroll values('Viraj',25000,'2022-04-14'),('Vaibhav',60000,'2021-08-15'),('Varad',15000,'2023-04-20');
+insert into employee_payroll values('Viraj',25000,'2022-04-14'),('Vaibhav',60000,'2021-08-15'),('Varad',15000,'2023-04-20'),('Mayuri',15000,'2021-01-18'),('Mitali',10000,'2020-04-10');
 
 UC4 : Retrieve all the employee_payroll data
 
@@ -27,12 +27,34 @@ SELECT salary FROM employee_payroll WHERE name = 'Viraj';
 
 SELECT * FROM employee_payroll WHERE startdate BETWEEN CAST('2022-01-01' AS DATE) AND GETDATE();
 
-UC6- Ability to add Gender to employee_payroll table
+UC6 : Ability to add Gender to employee_payroll table
 
 ALTER TABLE employee_payroll ADD gender VARCHAR(1);
 
 UPDATE employee_payroll set gender='M' where name = 'Viraj';
 UPDATE Employee_Payroll set Gender='M' where name = 'Vaibhav' or name = 'Varad';
+UPDATE Employee_Payroll set Gender='F' where name = 'Mayuri' or name = 'Mitali';
+
+UC7 : Ability to find sum,average,min,max & count
+
+SELECT SUM(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender;
+SELECT SUM(salary) FROM employee_payroll WHERE gender = 'F' GROUP BY gender;
+
+SELECT AVG(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender;
+SELECT AVG(salary) FROM employee_payroll WHERE gender = 'F' GROUP BY gender;
+
+SELECT gender, MIN(salary) FROM employee_payroll GROUP BY gender;
+
+SELECT gender, MAX(salary) FROM employee_payroll GROUP BY gender;
+
+SELECT gender, COUNT(name) FROM employee_payroll GROUP BY gender;
+
+
+
+
+
+
+
 
 
 
